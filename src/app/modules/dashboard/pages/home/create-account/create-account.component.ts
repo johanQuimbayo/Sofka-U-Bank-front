2976a30b3d7f2ca93df54f064960dd4a8066bf0c
Sergio Ismael Ayala Hernandez <sergio.ayala@sofka.local>
2025-01-28 18:58:30 -0500

    se agregan funcionalidades de recibir al usuario y crear cuentas sin estilos

---
+++ src/app/modules/dashboard/pages/home/create-account/create-account.component.ts
@@ -0,0 +1,46 @@
+import {Component, EventEmitter, Inject, Output} from '@angular/core';
+import {AccountService} from "../../../../../services/account/account.service";
+
+interface Account {
+  type: string;
+  customerId: number;
+  balance: number;
+}
+@Component({
+  selector: 'app-create-account',
+  templateUrl: './create-account.component.html',
+  styleUrls: ['./create-account.component.css']
+})
+export class CreateAccountComponent {
+  accountType: string = 'CHECKING';
+  balance: number = 0;
+
+  @Output() closeModal = new EventEmitter<void>();
+  @Output() accountCreated = new EventEmitter<void>();
+
+  constructor(private accountService: AccountService) {}
+
+  close(): void {
+    this.closeModal.emit();
+  }
+
+  createAccount(): void {
+    const newAccount = {
+      type: this.accountType,
+      customerId: 1, // Asumiendo que usas el mismo customerId del HomeComponent
+      balance: this.balance
+    };
+
+    // @ts-ignore
+    this.accountService.createAccount(newAccount).subscribe({
+      next: () => {
+        this.accountCreated.emit();
+        this.closeModal.emit();
+      },
+      error: (error) => {
+        console.error('Error creating account:', error);
+        // Aquí podrías agregar manejo de errores
+      }
+    });
+  }
+}
--- src/app/modules/dashboard/pages/home/home.component.ts
+++ src/app/modules/dashboard/pages/home/home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from "../../../../services/account/account.service";
+import { CreateAccountComponent } from "./create-account/create-account.component";
+
+interface Account {
+  id?: number;
+  type: string;
+  customerId: number;
+  balance: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  accounts: Account[] = [];
+  customerId: number = 1;
+  showCreateAccountModal: boolean = false;
 
+  constructor(private accountsService: AccountService) {}
+
+  ngOnInit(): void {
+    this.loadAccounts();
+  }
+
+  loadAccounts(): void {
+    this.accountsService.getAccounts(this.customerId).subscribe(data => {
+      this.accounts = data;
+    });
+  }
+
+  openCreateAccountModal(): void {
+    this.showCreateAccountModal = true;
+  }
+
+  closeCreateAccountModal(): void {
+    this.showCreateAccountModal = false;
+  }
 }
