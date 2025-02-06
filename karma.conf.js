6b8095b6c9e7353346fc175e1648698fa76cbb43
NVFox <davidroa8@hotmail.com>
2025-02-05 23:06:30 -0500

    removed sonar-scanner dep and adjusted karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
             require('@angular-devkit/build-angular/plugins/karma'),
             require('karma-sonarqube-reporter')
         ],
-        reporters: ['progress', 'kjhtml', 'sonarqube', 'coverage'],
+        reporters: ['progress', 'sonarqube', 'coverage'],
         browsers: ['Chrome'],
         coverageReporter: {
             dir: require('path').join(__dirname, 'coverage'),
