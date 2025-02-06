0f6df87057903d916e210b1950741af1c6eb3d70
NVFox <davidroa8@hotmail.com>
2025-02-05 22:02:41 -0500

    add impl of integration of kama and sonar pipeline

---
+++ karma.conf.js
@@ -0,0 +1,41 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
+module.exports = function(config) {
+    config.set({
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage'),
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-sonarqube-reporter')
+        ],
+        reporters: ['progress', 'kjhtml', 'sonarqube', 'coverage'],
+        browsers: ['Chrome'],
+        coverageReporter: {
+            dir: require('path').join(__dirname, 'coverage'),
+            reporters: [
+                { type: 'lcov', subdir: '.' },
+                { type: 'text-summary' }
+            ]
+        },
+        sonarqubeReporter: {
+            basePath: 'src/app',
+            outputFolder: 'reports',
+            filePattern: '**/*spec.ts',
+            encoding: 'utf-8',
+            legacyMode: false,
+            reportName: () => {
+                return 'sonarqube_report.xml';
+            }
+        },
+        singleRun: true,
+        customLaunchers: {
+            HeadlessChrome: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox', '--disable-gpu']
+            }
+        }
+    });
+};
